c664e36689ddab7d0f126ebbcf03570c60a4f711
Daniel <dannysht27@gmail.com>
2023-02-03 10:29:47 +0100

    Added miscelaneous folder

---
+++ 00_Miscellaneous/variables.js
@@ -0,0 +1,14 @@
+const me = {
+    /* key:value === key-value pairs */
+}
+me.name = "Daniel"
+
+const hobbyOne = "Basketball"
+const hobbyTwo = 'Video games'
+const hobbyThree = `Gym`
+
+
+const hobbies = ["Basketball"]
+hobbies.push = "Video games"
+me.hobbies = hobbies
+console.log(me);
