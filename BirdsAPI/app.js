b5a4488882bb901565db9c5f6a8ebd743f814d44
Daniel <dannysht27@gmail.com>
2023-02-23 20:17:08 +0100

    added POST, PUT, DELETE endpoints in BirdsAPI

--- BirdsAPI/app.js
+++ BirdsAPI/app.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const app = express()
 
-const birds = [{name: "parrot"}, {name: "eagle"}]
+app.use(express.json())
+
+const birds = [{id: 1 , name: "parrot"}, {id: 2, name: "eagle"}]
+let id = birds[birds.length-1].id + 1
 
 app.get("/birds", (req, res)=>
 {
@@ -10,13 +13,65 @@ app.get("/birds", (req, res)=>
 
 app.get("/birds", (req, res)=>
 {
-    res.send(birds)
+    res.send({data:birds})
 })
 
 app.get("/birds/:id", (req, res) =>
 {
-    res.send({id : req.params.id})
+    const foundBird = birds.find(bird => bird.id === Number(req.params.id))
+    res.send({data : foundBird})
 })
 
+app.post("/birds", (req, res) =>
+{
+    const newBird = 
+    {
+        id: id,
+        ...req.body
+    }
+    birds.push(newBird)
+    res.send({data:birds})
+})
 
-app.listen(8080)
+app.put("/birds/:id", (req, res) =>
+{
+    let birdToChange = birds.find(bird => bird.id === Number(req.params.id))
+    const indexOfBird = birds.indexOf(birdToChange)
+    if(indexOfBird !== -1)
+    {
+        birdToChange =
+        {
+            ...birdToChange,
+            ...req.body
+        }
+        birds[indexOfBird] = birdToChange
+        res.send({data:birdToChange})
+    }
+    else
+    {
+        res.send({message: `No bird with id ${req.params.id}`})
+    }
+})
+
+app.delete("/birds/:id", (req, res) =>
+{
+    
+    const birdToDelete = birds.find(bird => bird.id === Number(req.params.id))
+    const indexOfBird = birds.indexOf(birdToDelete)
+    if(indexOfBird !== -1)
+    {
+        birds.splice(indexOfBird, 1)
+        res.send({message: `Deleted bird with id ${req.params.id}`})
+    }
+    else
+    {
+        console.log(birds);
+        res.send({message: `No bird with id ${req.params.id}`})
+    }
+    
+})
+
+app.listen(8080), () =>
+{
+    console.log("App is running on port", 8080);
+}
